1aa89163d285c67ac4035a8ddd19f7229deb0df1
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-05-07 11:59:36 -0300

    test 14

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Verificar se o módulo distutils está instalado
-if ! python3.12 -c "import distutils" &> /dev/null; then
-    echo "Instalando o módulo distutils..."
-    pip3.12 install distutils
+# Instalar python3-distutils se não estiver instalado
+if ! dpkg -s python3-distutils &> /dev/null; then
+    echo "Instalando python3-distutils..."
+    sudo apt-get update
+    sudo apt-get install -y python3-distutils
 fi
 
 # Instalar os requisitos do projeto
