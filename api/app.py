883eab958660af1f64b53866514877f82d24f945
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-05-07 15:24:25 -0300

    test 32

--- api/app.py
+++ api/app.py
@@ -1,17 +1,25 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import spacy
-from werkzeug.urls import url_encode
 
 app = Flask(__name__)
 CORS(app, origins='*')
-nlp = spacy.load("en_core_web_sm")  # Carrega o modelo spaCy
+
+# Carrega o modelo spaCy
+try:
+    nlp = spacy.load("en_core_web_sm")
+except Exception as e:
+    print(f"Erro ao carregar o modelo spaCy: {e}")
 
 @app.route('/vectorize', methods=['POST'])
 def vectorize():
     texts = request.get_json().get('texts', [])
     # Processa os textos e obtém os vetores médios dos tokens
-    vectors = [nlp(text).vector for text in texts if text.strip()]
+    vectors = []
+    for text in texts:
+        if text.strip():  # Verifica se o texto não está vazio
+            doc = nlp(text)
+            vectors.append(doc.vector)
     return jsonify([vector.tolist() for vector in vectors])  # Retorna vetores como lista para facilitar serialização
 
 if __name__ == '__main__':
