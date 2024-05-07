7704b575dfb45d760105aa5b7cc8edc5d8021875
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-05-07 12:37:14 -0300

    test 18

--- api/app.py
+++ api/app.py
@@ -1,16 +1,16 @@
 from flask import Flask, request, jsonify
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, origins='*')
-vectorizer = CountVectorizer(stop_words='english')
+vectorizer = TfidfVectorizer(stop_words='english')
 
 @app.route('/vectorize', methods=['POST'])
 def vectorize():
     texts = request.get_json().get('texts', [])
     X = vectorizer.fit_transform(texts)
-    return jsonify(X.toarray().tolist())
+    return jsonify(X.toarray().tolist())  # Retorna vetores como lista para facilitar serialização
 
 if __name__ == '__main__':
     app.run(port=5003)
