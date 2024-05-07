946fbabc8d331c3f22d35c97bb65444d2068b643
Sousa <114963739+ESousa97@users.noreply.github.com>
2024-05-07 11:17:12 -0300

    test 1

--- next.config.js
+++ next.config.js
@@ -3,8 +3,12 @@ module.exports = {
   async rewrites() {
     return [
       {
+        source: '/vectorize',
+        destination: process.env.NODE_ENV === 'production' ? 'https://service-vectorization-api.vercel.app/vectorize' : 'http://localhost:5003/vectorize',
+      },
+      {
         source: '/api/:path*',
-        destination: process.env.NODE_ENV === 'production' ? 'https://your-vercel-deployment-url/api/:path*' : 'http://localhost:5328/:path*',
+        destination: process.env.NODE_ENV === 'production' ? 'https://service-vectorization-api.vercel.app/api/:path*' : 'http://localhost:5003/:path*',
       },
     ]
   },
